d633e28a514255a81f995f66e17c8e48a97a05ee
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-09-09 21:34:35 +0200

    Reduce build options for libadwaita
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- docs/install_libadwaita.sh
+++ docs/install_libadwaita.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://gitlab.gnome.org/GNOME/libadwaita
+git clone https://gitlab.gnome.org/GNOME/libadwaita -Dvapi=false -Dtests=false -Dexamples=false
 git checkout fcf62885bb28ec1da95d209c7de4bdaa3aef8746
 
 cd libadwaita
