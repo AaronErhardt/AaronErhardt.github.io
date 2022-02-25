1a22d3cd3e8ff4b1167c926be4f34c6ea7730376
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-02-25 19:13:52 +0100

    Add build script for libpanel
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

---
+++ docs/install_libpanel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Return on error
+set -e
+
+git clone --depth 1 https://gitlab.gnome.org/chergert/libpanel
+
+cd libadwaita
+
+meson _build --prefix=/usr -Dvapi=false
+ninja -C _build install
+
+cd ..
